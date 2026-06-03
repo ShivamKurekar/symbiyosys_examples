6b5dcc514f3dfe372c91d4a950b7a004a8dc3924
ShivamKurekar <kurekarshivam@gmail.com>
2026-06-03 11:41:31 +0530

    feat: counter: tested ifdef format
    Signed-off-by: ShivamKurekar <kurekarshivam@gmail.com>

--- test_counter/counter.sv
+++ test_counter/counter.sv
@@ -11,4 +11,32 @@ always @(posedge clk) begin
         cnt <= cnt + 1;
 end
 
+`ifdef FORMAL
+
+reg f_past_valid;
+
+initial f_past_valid = 0;
+
+always @(posedge clk)
+begin
+    f_past_valid <= 1;
+
+    if (f_past_valid) begin
+
+        // Check reset behavior
+        if ($past(rst))
+            assert(cnt == 0);
+
+        // Check counting behavior
+        if (!$past(rst))
+            assert(
+                (($past(cnt) != 3) && (cnt == $past(cnt)+1))
+                ||
+                (($past(cnt) == 3) && (cnt == 0))
+            );
+    end
+end
+
+`endif
+
 endmodule
